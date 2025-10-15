de55bd3874e006d5d9d9f5b7170d4bf4c384581d
priantim08-lab <priantim08@gmail.com>
2025-10-15 10:03:47 -0600

    Add files via upload

---
+++ practica 5 prianti.cs
@@ -0,0 +1,36 @@
+namespace practica_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double Radius, Height;
+            double BaseArea, LateralArea, TotalArea;
+            double volume;
+
+            Radius = double.Parse(textBox1.Text);
+            Height = double.Parse(textBox2.Text);
+
+            BaseArea = Radius * Radius * Math.PI;
+            LateralArea = 2 * Math.PI * Radius * Height;
+            TotalArea = 2 * Math.PI * Radius * (Height + Radius);
+            volume = Math.PI * Radius * Radius * Height;
+
+
+            textBox3.Text = BaseArea.ToString("0.##");
+            textBox4.Text = LateralArea.ToString("0.##");
+            textBox5.Text = TotalArea.ToString("0.##");
+            textBox6.Text = volume.ToString("0.##");
+        }
+    }
+}
